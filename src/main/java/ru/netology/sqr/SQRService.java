7ac25c72503f683684873cd69189d205cc443531
Shuvaeva Anastasia <sunny.shuvaeva@mail.ru>
2021-10-20 19:34:19 +0300

    gg

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,13 +4,15 @@ public class SQRService {
 
     public int squaresAreCertainRangeOfNumbers(int initial, int past) {
         int counter = 0;
-        for (int i = 10; i <= 99; i++  )
-    {
-            if (i * i >= initial && i * i <= past); {
-        counter ++;
-            }
+        for (int i = 10; i <= 99; i++  ) {
+
+            if (i * i >= initial && i * i <= past){
+
+        counter = counter +1;
         }
-        return counter;
-    }
 }
+    return counter;
+          }
+    }
+
 
